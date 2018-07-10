890a3161336ead371aea8312d3f437f9118a888f
Andy Ly <andywindsong@gmail.com>
2018-07-09 19:37:33 -0700

    POST test

--- test/test-recipes.js
+++ test/test-recipes.js
@@ -20,6 +20,7 @@ describe('Recipes', function () {
     return closeServer();
   });
 
+  // GET: make request to recipes and inspect response if correct
   it('should list items on GET', function () {
     return chai
       .request(app)
@@ -31,11 +32,31 @@ describe('Recipes', function () {
         // since we made at least 1 item on app load
         expect(res.body.length).to.be.at.least(1);
         // should have id, name, ingredients
-        const expectedKeys = ['id', 'name', 'checked'];
+        const expectedKeys = ['id', 'name', 'ingredients'];
         res.body.forEach(function (item) {
           expect(item).to.be.a('object');
           expect(item).to.include.keys(expectedKeys);
         })
+      });
+  });
+
+  // POST: make post req and check if has right status code and requirements
+  it('should add item on POST', function () {
+    const newItem = {name: 'pizza bites', ingredients: ['pillsbury dough', 'peperoni']};
+    return chai
+      .request(app)
+      .post('/recipes')
+      .send(newItem)
+      .then(function (res) {
+        expect(res).to.have.status(201);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.include.keys('id', 'name', 'ingredients');
+        expect(res.body.id).to.not.equal(null);
+        // response should be deep equal to newItem if assign id to it
+        expect(res.body).to.deep.equal(
+          Object.assign(newItem, {id: res.body.id})
+        );
       });
   });
 
